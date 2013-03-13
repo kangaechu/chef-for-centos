fd736e53d92fd0371b15cb766712aebb55176aff
Satoshi Tanaka <kangae2@gmail.com>
2013-03-13 16:07:17 +0900

    Virtualbox recipe修正

--- cookbooks/virtualbox/recipes/default.rb
+++ cookbooks/virtualbox/recipes/default.rb
@@ -30,22 +30,24 @@ filename = "VBoxGuestAdditions_#{versionServer}"
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{filename}.iso" do
   source "http://download.virtualbox.org/virtualbox/#{versionServer}/#{filename}.iso"
-  not_if {File.exists?("#{Chef::Config[:file_cache_path]}/#{filename}.iso") or
-          File.exists?("/opt/VBoxGuestAdditions-" + versionServer + "/bin")}
+  not_if do
+    ::File.exists?("#{Chef::Config[:file_cache_path]}/#{filename}.iso") or
+    ::File.exists?("/opt/VBoxGuestAdditions-" + versionServer + "/bin")
+  end
 end
 
 bash "install Virtualbox Guest Additions" do
   user "root"
   cwd "/tmp"
-  flags "-x -e"
   code <<-EOH
   mkdir -p /mnt/#{filename}
-  mount #{filename}.iso -o loop /mnt/#{filename}
+  mount #{Chef::Config[:file_cache_path]}/#{filename}.iso -o loop /mnt/#{filename}
   sh /mnt/#{filename}/VBoxLinuxAdditions.run --nox11
   umount /mnt/#{filename}
   rmdir /mnt/#{filename}
-  rm -rf #{filename}.iso
   EOH
-  not_if {! File.exists?("#{Chef::Config[:file_cache_path]}/#{filename}.iso") or
-          File.exists?("/opt/VBoxGuestAdditions-" + versionServer + "/bin")}
+  not_if do
+    ! ::File.exists?("#{Chef::Config[:file_cache_path]}/#{filename}.iso") or
+    ::File.exists?("/opt/VBoxGuestAdditions-" + versionServer + "/bin")
+  end
 end
